839ae90d12e7d3bfd8bf69d77717e1f7948a9c7f
Урванов Егор <urvanov@corp.sputnik.ru>
2017-04-28 15:30:14 +0300

    Реализовано подавление шумов в номинальных данных

--- CLOPE.py
+++ CLOPE.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Класс, описывающий кластер
-class CClaster:
+class CCluster:
     # Гистограмма
     Histogram = {}
     # Площадь гистограммы
@@ -57,7 +57,6 @@ class CClaster:
     # выдаст неверный результат
     def RemoveTransaction(self, transaction):
         for item in transaction:
-            self.Histogram[item] -= 1
             if self.Histogram[item] == 0:
                 del self.Histogram[item]
         self.Area -= float(len(transaction))
@@ -67,45 +66,65 @@ class CClaster:
 
 # Класс, описывающий работу с данными
 class CData:
+    # Максимальный номер кластера
+    MaxClusterNumber = 0
     # Список кластеров
-    Clasters = [] # CClaster
+    Clusters = {} # CCluster
     # Количество добавленных транзакций
     CountTransactions = 0
     # Словарь. Ключи -- номер транзакции. Значение -- номер кластера
     Transaction = {}
     # Номер итерации
     Iteration = 0
+    # Номера шумовых кластеров
+    # Данный объект необходим для того, чтобы не брать во внимание те объкты, которые были отнесены к шумовым
+    NoiseClusters = {}
 
     def __init__(self):
-        self.Clasters = []
+        self.Clusters = {}
+        self.NoiseClusters = {}
         self.CountTransactions = 0
         self.Iteration = 0
         self.Transaction = {}
+        self.MaxClusterNumber = 0
 
-    # Рассчитывается изменение Goal, которое получит целевая функция при добавлении транзакции к кластеру clasterNumber.
+    # Рассчитывается изменение Goal, которое получит целевая функция при добавлении транзакции к кластеру clusterNumber.
     # Кластер, доставляющий максимальное значение функции, будет искомым кластером (в который следует добавить транзакцию)
     # Input parametres:
     # transaction -- транзакция (список объектов)
-    # clasterNumber -- номер кластера, приращение для которого рассчитывается
+    # clusterNumber -- номер кластера, приращение для которого рассчитывается
     # r -- отталкивание в смысле CLOPE
     # Returned value:
-    # Возвращает значение изменения целевой функции при добавлении transaction к кластеру clasterNumber
-    def DeltaAddTransaction(self, transaction, clasterNumber, r):
-        area = self.Clasters[clasterNumber].Area + len(transaction)
-        width = self.Clasters[clasterNumber].Width
+    # Возвращает значение изменения целевой функции при добавлении transaction к кластеру clusterNumber
+    def DeltaAddTransaction(self, transaction, clusterNumber, r):
+        area = self.Clusters[clusterNumber].Area + len(transaction)
+        width = self.Clusters[clusterNumber].Width
         for item in transaction:
-            if not (item in self.Clasters[clasterNumber].Histogram):
+            if not (item in self.Clusters[clusterNumber].Histogram):
                 width += 1
         if width != 0:
-            newDeltaValue = area * (self.Clasters[clasterNumber].CountTransactions + 1) / (width ** r)
+            newDeltaValue = area * (self.Clusters[clusterNumber].CountTransactions + 1) / (width ** r)
         else:
             newDeltaValue = 0
-        if self.Clasters[clasterNumber].Width != 0:
-            oldDeltaValue = self.Clasters[clasterNumber].Area * self.Clasters[clasterNumber].CountTransactions / (
-            self.Clasters[clasterNumber].Width ** r)
+        if self.Clusters[clusterNumber].Width != 0:
+            oldDeltaValue = self.Clusters[clusterNumber].Area * self.Clusters[clusterNumber].CountTransactions / (
+            self.Clusters[clusterNumber].Width ** r)
         else:
             oldDeltaValue = 0
         return newDeltaValue - oldDeltaValue
+
+    # Функция удаления шума. Все кластеры, размер которых больше limit остаются
+    # Input parametres:
+    # limit -- уровень шума кластеров
+    def NoiseReduction(self, limit):
+        # Удаляем все пустые кластеры
+        newClusters = {}
+        for item in self.Clusters:
+            if self.Clusters[item].CountTransactions > limit:
+                newClusters[item] = self.Clusters[item]
+            else:
+                self.NoiseClusters[item] = True
+        self.Clusters = newClusters
 
     # Вычисление целевой функции для всех уже сформированных кластеров
     # Используется при модификации кластеров, либо их инициализации
@@ -116,7 +135,7 @@ class CData:
     def GetGoalFunction(self, r):
         measure = 0.0
         # Перебираем все кластеры и для каждого рассчитываем его вес. Все веса суммируются в общую метрику
-        for item in self.Clasters:
+        for item in self.Clusters:
             if item.Width == 0:
                 # print "test"
                 pass
@@ -137,12 +156,15 @@ class CData:
     # Возвращает значение, отвечающие наличию изменения кластера или его отсутствию
     def ModifyTransactions(self, transaction, id, repulsion = 2, isSaveHistory = True):
         # Смотрим, где данная транзакция лежит сейчас
-        numberCluster = self.Transaction[id]
+        clusterNumber = self.Transaction[id]
+        # Если транзакция относится к шумовому кластеру, то не пытаемся её поменять
+        if clusterNumber in self.NoiseClusters:
+            return 0
         # Извлекаем транзакцию из текущего кластера
-        self.Clasters[numberCluster].RemoveTransaction(transaction)
+        self.Clusters[clusterNumber].RemoveTransaction(transaction)
         # Рассматриваем транзакцию как вновь пришедшую и добавляем в тот кластер, где значение целевой функции доставит
         # максимум
-        return int(self.AddNewTransaction(transaction, id, repulsion, isSaveHistory) != numberCluster)
+        return int(self.AddNewTransaction(transaction, id, repulsion, isSaveHistory) != clusterNumber)
 
     # Добавление новой транзакции
     # Пытаемся перераспределить транзакцию (transaction) с номером id в другой класс так, чтобы целевая функция приняла
@@ -159,34 +181,34 @@ class CData:
         maxValue = -1
         maxValueIndex = -1
         self.CountTransactions += 1
-        index = 0
-        # Вычисляем максимальное значение изменения целевой функции
-        for clasterNumber in range(0, len(self.Clasters)):
-            delta = self.DeltaAddTransaction(transaction, clasterNumber, r)
+        # Ищем кластер, на котором будет достигнуто максимальное значение изменения целевой функции
+        for clusterNumber in self.Clusters:
+            delta = self.DeltaAddTransaction(transaction, clusterNumber, r)
             if delta > 0 and delta > maxValue:
-                maxValueIndex = index
+                maxValueIndex = clusterNumber
                 maxValue = delta
-            index += 1
         # Добавляем транзакцию в новый кластер и смотрим на результат
-        self.Clasters.append(CClaster())
-        valueGoalFunction = self.DeltaAddTransaction(transaction, len(self.Clasters) - 1, r)
+        self.Clusters[self.MaxClusterNumber] = CCluster()
+        valueGoalFunction = self.DeltaAddTransaction(transaction, self.MaxClusterNumber, r)
         if valueGoalFunction > maxValue:
-            maxValueIndex = index
+            maxValueIndex = self.MaxClusterNumber
+        self.MaxClusterNumber += 1
 
         # Запоминаем, в каком кластере лежит текущая транзакция
         self.Transaction[id] = maxValueIndex
-        if maxValueIndex != len(self.Clasters) - 1:
-            self.Clasters.pop()
+        if maxValueIndex != len(self.Clusters) - 1:
+            self.MaxClusterNumber -= 1
+            del self.Clusters[self.MaxClusterNumber]
         else:
             if isSaveHistory:
-                self.Clasters[-1].HistoryCountTransact = np.array([0] * len(self.Clasters[0].HistoryCountTransact))
+                self.Clusters[-1].HistoryCountTransact = np.array([0] * len(self.Clusters[0].HistoryCountTransact))
 
         # Добавляем транзакцию в необходимый кластер
-        self.Clasters[maxValueIndex].AddTransaction(transaction)
+        self.Clusters[maxValueIndex].AddTransaction(transaction)
 
         if isSaveHistory:
-            for itemClaster in self.Clasters:
-                itemClaster.HistoryCountTransact = np.append(itemClaster.HistoryCountTransact, itemClaster.CountTransactions)
+            for itemCluster in self.Clusters:
+                itemCluster.HistoryCountTransact = np.append(itemCluster.HistoryCountTransact, itemCluster.CountTransactions)
 
         return maxValueIndex
 
@@ -196,13 +218,18 @@ class CData:
     # isPrint -- нужно ли печатать информацию о ходе выполнения (0 -- не нужно, если > 0 -- печатаем каждый isPrint раз)
     # repulsion -- вещественное число, обозначающие отталкивание кластеров в смысле CLOPE
     # isSaveHistory -- флаг, выставляемый при необходимости записи истории количества транзакций
-    def Init(self, data, isPrint, repulsion = 2, isSaveHistory = True):
+    def Init(self, data, isPrint, repulsion = 2, isSaveHistory = True, isNoiseReduction = 0):
         index = 0
         for item in data:
             self.AddNewTransaction(data[item], item, repulsion, isSaveHistory)
             index += 1
             if isPrint > 0 and index % isPrint == 0:
-                print("Итерация: ", self.Iteration, ". Номер шага", index, ". Число кластеров: ", len(self.Clasters))
+                print("Итерация: ", self.Iteration, ". Номер шага", index, ". Число кластеров: ", len(self.Clusters))
+
+        # Удаляем все шумовые кластеры (после инициализации не бывает пустых классов, поэтому знак строго больше)
+        if isNoiseReduction > 0:
+            self.NoiseReduction(isNoiseReduction)
+
         self.Iteration = 1
 
     # Выполнение алгоритма. Выполнение следующего шага
@@ -213,23 +240,27 @@ class CData:
     # isSaveHistory -- флаг, выставляемый при необходимости записи истории количества транзакций
     # Returned parameter:
     # Возвращается число операций по перенесению транзакции из кластера в кластер
-    def NextStep(self, data, isPrint, repulsion = 2, isSaveHistory = True):
+    def NextStep(self, data, isPrint, repulsion = 2, isSaveHistory = True, isNoiseReduction = 0):
         index = 0
         eps = 0
         for item in data:
             eps += self.ModifyTransactions(data[item], item, repulsion, isSaveHistory)
             index += 1
             if isPrint > 0 and index % isPrint == 0:
-                print("Итерация: ", self.Iteration, ". Номер шага", index, ". Число кластеров: ", len(self.Clasters))
+                print("Итерация: ", self.Iteration, ". Номер шага", index, ". Число кластеров: ", len(self.Clusters))
         self.Iteration += 1
+
+        # Удаляем все пустые (или шумовые, если isNoiseReduction > 0) кластеры
+        if isNoiseReduction >= 0:
+            self.NoiseReduction(isNoiseReduction)
         return eps
 
     # Рисуем график, демонстрирующий количество транзакций в различных классах
     def PrintHistoryCount(self):
-        for itemClaster in self.Clasters:
+        for itemCluster in self.Clusters:
             x = np.array(range(0, len(self.Transaction) * self.Iteration))
-            if len(itemClaster.HistoryCountTransact) != 0:
-                y = itemClaster.HistoryCountTransact
+            if len(itemCluster.HistoryCountTransact) != 0:
+                y = itemCluster.HistoryCountTransact
             else:
                 y = [0] * len(self.Transaction) * self.Iteration
             plt.plot(x, y)
--- testVk.py
+++ testVk.py
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
 
+import copy
 import CLOPE
 import pickle
 
 
-dataGroups = []
-
-with open('data/CLOPE_users.r=1.009.stopLimit=300.pickle', 'rb') as f:
-    clopeLoad = pickle.load(f)
-
 with open('data/users.pickle', 'rb') as f:
     dataGroups = pickle.load(f)
 
-repulsion = 1.03
+dataNew = {}
+index = 0
+for item in dataGroups:
+    if index > 0 and index > 50000:
+        break
+    dataNew[item] = dataGroups[item]
+    index += 1
+dataGroups = copy.deepcopy(dataNew)
+repulsion = 1.002
+noiseLimit = 10
 isSaveHistory = False
-iter = 2000
+iter = 10000
 countTransfer = 1000000
 stopLimit = 300
 
 clope = CLOPE.CData()
-clope.Init(dataGroups, iter, repulsion, isSaveHistory)
-print("Инициализация завершена")
+clope.Init(dataGroups, iter, repulsion, isSaveHistory, noiseLimit)
+print("Инициализация завершена. Число кластеров: ", len(clope.Clusters))
 while countTransfer > stopLimit:
-    countTransfer = clope.NextStep(dataGroups, iter, repulsion, isSaveHistory)
-    print("Число перемещений между кластерами", countTransfer)
+    countTransfer = clope.NextStep(dataGroups, iter, repulsion, isSaveHistory, noiseLimit)
+    print("Число перемещений между кластерами", countTransfer, ". Число кластеров: ", len(clope.Clusters))
 
 with open('data/CLOPE_users' + '.r=' + str(repulsion) + '.stopLimit=' + str(stopLimit) + '.pickle', 'wb') as f:
     pickle.dump(clope, f)
